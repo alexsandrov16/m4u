cec0b3c700ae8194d4d4291b104f781a172b5b67
alexsandrov16 <alejandro920131@gmail.com>
2022-09-06 16:29:50 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+const M4U = {
+    toggleSidebar: function () {
+        document.querySelector('aside').classList.toggle('toggle');
+        document.querySelector('.scrim').classList.toggle('toggle');
+    }
+}
